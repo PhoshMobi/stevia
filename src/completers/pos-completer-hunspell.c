PhoshMobi/stevia

commit c10ff17c0e0eca5c78eeeae0a10d543f4c7a791e

treewide: Move this years additions to Phosh e.V
Signed-off-by: Guido Günther <agx@sigxcpu.org>

src/completers/pos-completer-hunspell.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2022 Purism SPC
- *               2025 The Phosh Developers
+ *               2025 Phosh e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-completer-base.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Phosh Developers
+ * Copyright (C) 2025 Phosh e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-completer-base.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Phosh Developers
+ * Copyright (C) 2025 Phosh e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */

src/pos-completions-box.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Phosh Developers
+ * Copyright (C) 2025 Phosh e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-completions-box.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Phosh Developers
+ * Copyright (C) 2025 Phosh e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */

src/pos-emoji-db.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Phosh Developers
+ * Copyright (C) 2025 Phosh e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-emoji-db.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Phosh Developers
+ * Copyright (C) 2025 Phosh e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */

src/pos-indicator-popup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Phosh Developers
+ * Copyright (C) 2025 Phosh e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

commit 482d7412fee47f9447f60a4ab77ee1a8d297bfd3

treewide: Fix copyright
It's Phosh.mobi e.V., not Phosh e.V.

Gbp-Dch: Ignore
Fixes: c10ff17c0 ("treewide: Move this years additions to Phosh e.V")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/238>

src/completers/pos-completer-hunspell.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2022 Purism SPC
- *               2025 Phosh e.V.
+ *               2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-completer-base.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-completer-base.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */

src/pos-completions-box.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-completions-box.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */

src/pos-emoji-db.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-emoji-db.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */

src/pos-indicator-popup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/pos-indicator-popup.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */

src/pos-osk-key.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2022 Purism SPC
- *               2025 Phosh e.V.
+ *               2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

tests/pos-test-completer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2025 Phosh e.V.
+ * Copyright © 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */

tests/test-completer-hunspell.c
@@ -1,5 +1,5 @@
 /*
- * Copyright: (C) 2025 Phosh e.V.
+ * Copyright: (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

tests/test-completer-presage.c
@@ -1,5 +1,5 @@
 /*
- * 2025 Phosh e.V.
+ * 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

tests/test-completers.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2025 Phosh e.V.
+ * Copyright © 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

tests/test-emoji-db.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2025 Phosh e.V.
+ * Copyright © 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

tests/test-osk-widget.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Phosh e.V.
+ * Copyright (C) 2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *