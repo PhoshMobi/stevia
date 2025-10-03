PhoshMobi/stevia

commit ad7bc05df16390f7a26e4c58e3a1a6949fa66925

size-manager: New object to calculate input surface size
The size manager takes the current outputs physical and logical
size into account to calculate height when the corresponding
gsettings are enabled.

This is currently limited to portrait mode.

We target the keyboard's key area height to be 4cm so keys are reasonably
sized. The target height for the dead zone at the bottom is 8mm. This
is useful on taller devices as otherwise when typing the bottom row requires
additional force as the center of gravity is far away from the supporting
fingers.

Signed-off-by: Guido Günther <agx@sigxcpu.org>

src/pos-size-manager.c
@@ -1,0 +1,289 @@
+/*
+ * Copyright (C) 2025 Phosh.mobi e.V.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#define G_LOG_DOMAIN "pos-size-manager"
+
+#include "pos-config.h"
+
+#include "phosh-osk-enums.h"
+#include "pos.h"
+
+#include <gtk/gtk.h>
+
+#define SCALING_SCHEMA_ID "sm.puri.phosh.osk"
+#define SCALING_KEY "scaling"
+
+/**
+ * PosSizeManager:
+ *
+ * Calculate a useful surface size
+ */
+
+enum {
+  PROP_0,
+  PROP_HEIGHT,
+  PROP_DEAD_ZONE,
+  PROP_LAST_PROP
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+struct _PosSizeManager {
+  GObject    parent;
+
+  PosOutput *output;
+  guint      height;
+  guint      dead_zone;
+
+  GSettings *settings;
+};
+G_DEFINE_TYPE (PosSizeManager, pos_size_manager, G_TYPE_OBJECT)
+
+
+static void
+set_height (PosSizeManager *self, guint height)
+{
+  if (self->height == height)
+    return;
+
+  self->height = height;
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_HEIGHT]);
+}
+
+
+static void
+set_dead_zone (PosSizeManager *self, guint dead_zone)
+{
+  if (self->dead_zone == dead_zone)
+    return;
+
+  self->dead_zone = dead_zone;
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_DEAD_ZONE]);
+}
+
+
+#define MIN_PORTRAIT_RATIO 0.4
+#define TARGET_PORTRAIT_PHYS_HEIGHT 40 /* mm */
+#define TARGET_PORTRAIT_BOTTOM_DEAD_ZONE 8 /* mm */
+
+static guint
+calc_height (PosOutput *output, guint *dead_zone, PhoshOskScalingFlags flags)
+{
+  guint osk_height = POS_INPUT_SURFACE_DEFAULT_HEIGHT;
+  double phys_dz;
+  double pixel_size;
+  double screen_logical_height;
+  double screen_physical_height;
+
+  g_assert (dead_zone);
+  *dead_zone = 0;
+
+  if (!output)
+    return osk_height;
+
+  if (!flags)
+    return osk_height;
+
+  if (!pos_output_is_portrait (output)) {
+    /* TODO: set small height on phone displays so there's more screen estate but
+     * use enough vertical space on landscape tablets */
+    return osk_height;
+  }
+
+  screen_logical_height = pos_output_get_logical_height (output);
+
+  /* vertical resolution not high enough to take up any extra space */
+  if (osk_height > screen_logical_height * MIN_PORTRAIT_RATIO)
+    return osk_height;
+
+  screen_physical_height = pos_output_get_transformed_phys_height (output);
+  /* vertical size not large enough for a larger OSK */
+  if (TARGET_PORTRAIT_PHYS_HEIGHT > screen_physical_height * MIN_PORTRAIT_RATIO)
+    return osk_height;
+
+  pixel_size = pos_output_get_logical_pixel_size (output);
+
+  if (flags & PHOSH_OSK_SCALING_AUTO_PORTRAIT)
+    osk_height = TARGET_PORTRAIT_PHYS_HEIGHT / pixel_size;
+
+  phys_dz = (MIN_PORTRAIT_RATIO * screen_physical_height) - (osk_height * pixel_size);
+
+  if (flags & PHOSH_OSK_SCALING_BOTTOM_DEAD_ZONE)
+    *dead_zone = MIN (TARGET_PORTRAIT_BOTTOM_DEAD_ZONE, phys_dz) / pixel_size;
+
+  return osk_height;
+}
+
+
+static void
+recalc_surface_size (PosSizeManager *self)
+{
+  guint height, dead_zone;
+  PhoshOskScalingFlags flags;
+
+  flags = g_settings_get_flags (self->settings, "scaling");
+  height = calc_height (self->output, &dead_zone, flags);
+  g_debug ("Target OSK height: %d, dead zone: %d", height, dead_zone);
+
+  set_height (self, height);
+  set_dead_zone (self, dead_zone);
+}
+
+
+static void
+on_output_configured (PosSizeManager *self, PosOutput *output)
+{
+  g_assert (POS_IS_SIZE_MANAGER (self));
+  g_assert (POS_IS_OUTPUT (output));
+
+  recalc_surface_size (self);
+}
+
+
+static void
+on_scaling_key_changed (PosSizeManager *self)
+{
+  g_assert (POS_IS_SIZE_MANAGER (self));
+
+  recalc_surface_size (self);
+}
+
+
+static void
+set_output (PosSizeManager *self, PosOutput *output)
+{
+  if (self->output)
+    g_signal_handlers_disconnect_by_data (self->output, self);
+
+  g_set_object (&self->output, output);
+
+  if (!self->output)
+    return;
+
+  g_signal_connect_swapped (self->output,
+                            "configured",
+                            G_CALLBACK (on_output_configured),
+                            self);
+}
+
+
+static void
+on_outputs_changed (PosSizeManager *self, GParamSpec *pspec, PosWayland *wayland)
+{
+  PosOutput *output;
+  GPtrArray *outputs;
+
+  if (pos_wayland_get_n_outputs (wayland) != 1) {
+    /* TODO: we can do better: if we didn't get a "surface enter" we can assume we're on
+       the same output */
+    set_output (self, NULL);
+    set_height (self, 0);
+    return;
+  }
+
+  /* Pick our output */
+  outputs = pos_wayland_get_outputs (wayland);
+  output = g_ptr_array_index (outputs, 0);
+  set_output (self, output);
+  on_output_configured (self, self->output);
+}
+
+
+static void
+pos_size_manager_get_property (GObject    *object,
+                               guint       property_id,
+                               GValue     *value,
+                               GParamSpec *pspec)
+{
+  PosSizeManager *self = POS_SIZE_MANAGER (object);
+
+  switch (property_id) {
+  case PROP_HEIGHT:
+    g_value_set_uint (value, self->height);
+    break;
+  case PROP_DEAD_ZONE:
+    g_value_set_uint (value, self->dead_zone);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+pos_size_manager_dispose (GObject *object)
+{
+  PosSizeManager *self = POS_SIZE_MANAGER (object);
+
+  set_output (self, NULL);
+  g_clear_object (&self->settings);
+
+  G_OBJECT_CLASS (pos_size_manager_parent_class)->dispose (object);
+}
+
+
+static void
+pos_size_manager_class_init (PosSizeManagerClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->get_property = pos_size_manager_get_property;
+  object_class->dispose = pos_size_manager_dispose;
+
+  /**
+   * PosSizeManager:height:
+   *
+   * The height in logical pixels the keyboard's key area should have.
+   */
+  props[PROP_HEIGHT] =
+    g_param_spec_uint ("height", "", "",
+                       0, G_MAXUINT,
+                       0,
+                       G_PARAM_READABLE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+  /**
+   * PosSizeManager:dead-zone:
+   *
+   * Empty space at the bottom of the keyboard.
+   */
+  props[PROP_DEAD_ZONE] =
+    g_param_spec_uint ("dead-zone", "", "",
+                       0, G_MAXUINT,
+                       0,
+                       G_PARAM_READABLE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
+}
+
+
+static void
+pos_size_manager_init (PosSizeManager *self)
+{
+  PosWayland *wayland = pos_wayland_get_default ();
+
+  self->settings = g_settings_new (SCALING_SCHEMA_ID);
+
+  g_signal_connect_object (self->settings,
+                           "changed::" SCALING_KEY,
+                           G_CALLBACK (on_scaling_key_changed),
+                           self,
+                           G_CONNECT_SWAPPED);
+  g_signal_connect_object (wayland,
+                           "notify::outputs",
+                           G_CALLBACK (on_outputs_changed),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  recalc_surface_size (self);
+}
+
+
+PosSizeManager *
+pos_size_manager_new (void)
+{
+  return g_object_new (POS_TYPE_SIZE_MANAGER, NULL);
+}

data/mobi.phosh.osk.gschema.xml
@@ -36,6 +36,15 @@
         disables this detection.
       </description>
     </key>
+
+    <key name="scaling" flags='mobi.phosh.osk.PhoshOskScalingFlags'>
+      <default>[]</default>
+      <summary>How to scale the on screen keyboard</summary>
+      <description>
+        These flags determine how the OSK adjusts to the screen size.
+      </description>
+    </key>
+
   </schema>
 
   <schema id='sm.puri.phosh.osk.EmojiPicker'

src/meson.build
@@ -79,6 +79,8 @@ libpos_sources = files(
   'pos-settings-panel.h',
   'pos-shortcuts-bar.c',
   'pos-shortcuts-bar.h',
+  'pos-size-manager.c',
+  'pos-size-manager.h',
   'pos-style-manager.c',
   'pos-style-manager.h',
   'pos-virtual-keyboard.c',

src/phosh-osk-enums.h
@@ -57,4 +57,19 @@ typedef enum {
   PHOSH_OSK_COMPLETION_SOURCE_KEYWORD = (1 << 1),
 } PhoshOskCompletionSourceFlags;
 
+/**
+ * PhoshOskScalingFlags:
+ * @PHOSH_OSK_SCALING_NONE: no scaling
+ * @PHOSH_OSK_SCALING_AUTO_PORTRAIT: automatic scaling in portrait mode
+ * @PHOSH_OSK_SCALING_AUTO_LANDSCAPE: automatic scaling in landscape mode
+ * @PHOSH_OSK_SCALING_BOTTOM_DEAD_ZONE: Dead zone at the bottom of the OSK
+ */
+typedef enum {
+  PHOSH_OSK_SCALING_NONE = 0, /*< skip >*/
+  PHOSH_OSK_SCALING_AUTO_PORTRAIT = (1 << 0),
+  /* TODO: not yet implemented */
+  /* PHOSH_OSK_SCALING_AUTO_LANDSCAPE = (1 << 1), */
+  PHOSH_OSK_SCALING_BOTTOM_DEAD_ZONE = (1 << 2),
+} PhoshOskScalingFlags;
+
 G_END_DECLS

src/pos-size-manager.h
@@ -1,0 +1,19 @@
+/*
+ * Copyright (C) 2025 Phosh.mobi e.V.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define POS_TYPE_SIZE_MANAGER (pos_size_manager_get_type ())
+
+G_DECLARE_FINAL_TYPE (PosSizeManager, pos_size_manager, POS, SIZE_MANAGER, GObject)
+
+PosSizeManager *pos_size_manager_new (void);
+
+G_END_DECLS

tests/meson.build
@@ -19,7 +19,15 @@ test_env.set('ASAN_OPTIONS', 'fast_unwind_on_malloc=0')
 # Useful when running locally and glib hits an assertion:
 #test_env.set('ASAN_OPTIONS', 'fast_unwind_on_malloc=0:disable_coredump=0:unmap_shadow_on_exit=1:abort_on_error=1')
 
-tests = ['grab-word', 'completer-hunspell', 'load-layouts', 'osk-widget', 'completers', 'emoji-db']
+tests = [
+  'grab-word',
+  'completer-hunspell',
+  'load-layouts',
+  'osk-widget',
+  'completers',
+  'emoji-db',
+  'size-manager',
+]
 foreach name : tests
   name = 'test-@0@'.format(name)
   test = executable(name, '@0@.c'.format(name), pie: true, dependencies: libpos_dep)

tests/test-size-manager.c
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2025 Phosh.mobi e.V.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#include "pos-config.h"
+
+#include "pos-size-manager.c"
+
+#include "phosh-osk-enums.h"
+#include "pos-output-priv.h"
+
+#include <glib.h>
+
+#define ALL_FLAGS (PHOSH_OSK_SCALING_AUTO_PORTRAIT | PHOSH_OSK_SCALING_BOTTOM_DEAD_ZONE)
+
+static void
+test_size_manager_landscape (void)
+{
+  PosOutput *output = g_object_new (POS_TYPE_OUTPUT, NULL);
+  guint dz;
+
+  pos_output_set_logical_width (output, 1000);
+  pos_output_set_logical_height (output, 100);
+
+  g_assert_cmpint (calc_height (output, &dz, ALL_FLAGS), ==, POS_INPUT_SURFACE_DEFAULT_HEIGHT);
+}
+
+
+static void
+test_size_manager_portrait_too_little_logical_height (void)
+{
+  PosOutput *output = g_object_new (POS_TYPE_OUTPUT, NULL);
+  guint dz;
+
+  pos_output_set_logical_width (output, 100);
+  pos_output_set_logical_height (output, 220);
+
+  g_assert_cmpint (calc_height (output, &dz, ALL_FLAGS), ==, POS_INPUT_SURFACE_DEFAULT_HEIGHT);
+}
+
+
+static void
+test_size_manager_portrait_too_little_physical_height (void)
+{
+  PosOutput *output = g_object_new (POS_TYPE_OUTPUT, NULL);
+  double pixel_size;
+  guint dz;
+
+  pos_output_set_logical_width (output, 360);
+  pos_output_set_logical_height (output, 720);
+  pos_output_set_physical_height (output, 60);
+  pixel_size = pos_output_get_logical_pixel_size (output);
+
+  g_assert_cmpfloat (pixel_size, >, 0.08);
+  g_assert_cmpint (calc_height (output, &dz, ALL_FLAGS), ==, POS_INPUT_SURFACE_DEFAULT_HEIGHT);
+}
+
+/* OnePlus 6T at scale 2.5 */
+static void
+test_size_manager_portrait_scale_up_op6t_at_250 (void)
+{
+  PosOutput *output = g_object_new (POS_TYPE_OUTPUT, NULL);
+  double pixel_size;
+  guint height, dz;
+
+  pos_output_set_logical_width (output, 1080 / 2.5);
+  pos_output_set_logical_height (output, 2340 / 2.5);
+  pos_output_set_physical_height (output, 145);
+  pixel_size = pos_output_get_logical_pixel_size (output);
+
+  g_assert_cmpfloat (pixel_size, >, 0.06);
+  height = calc_height (output, &dz, ALL_FLAGS);
+
+  g_assert_cmpint (height, ==, 258);
+  g_assert_cmpint (dz, ==, 51);
+}
+
+/* Librem 5 at scale 2 */
+static void
+test_size_manager_portrait_scale_up_l5_at_200 (void)
+{
+  PosOutput *output = g_object_new (POS_TYPE_OUTPUT, NULL);
+  double pixel_size;
+  guint height, dz;
+
+  pos_output_set_logical_width (output, 720 / 2.0);
+  pos_output_set_logical_height (output, 1440 / 2.0);
+  pos_output_set_physical_height (output, 130);
+  pixel_size = pos_output_get_logical_pixel_size (output);
+
+  g_assert_cmpfloat (pixel_size, >, 0.09);
+  height = calc_height (output, &dz, ALL_FLAGS);
+  g_assert_cmpint (height, ==, 221);
+  g_assert_cmpint (dz, ==, 44);
+}
+
+
+int
+main (int argc, char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/pos/size-manager/landscape", test_size_manager_landscape);
+  g_test_add_func ("/pos/size-manager/portrait/too_little_logical_height",
+                   test_size_manager_portrait_too_little_logical_height);
+  g_test_add_func ("/pos/size-manager/portrait/too_little_physical_height",
+                   test_size_manager_portrait_too_little_physical_height);
+  g_test_add_func ("/pos/size-manager/portrait/scale_up/op6t@250",
+                   test_size_manager_portrait_scale_up_op6t_at_250);
+  g_test_add_func ("/pos/size-manager/portrait/scale_up/l5@200",
+                   test_size_manager_portrait_scale_up_l5_at_200);
+
+  return g_test_run ();
+}

commit e3436eb8aaec7a7348f59e201c7af7bbbc0064b9

size-manager: Fall back to default hight
When we have multiple outputs and thus can't yet reliably determine
the optimal height fall back to the default height rather than 0.

This frees the upper layer from guessing a suitable height (which
it got wrong 😀).

Fixes: ad7bc05df ("size-manager: New object to calculate input surface size")

Closes: https://gitlab.gnome.org/World/Phosh/stevia/-/issues/75
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/244>

src/pos-size-manager.c
@@ -181,7 +181,7 @@ on_outputs_changed (PosSizeManager *self, GParamSpec *pspec, PosWayland *wayland
     /* TODO: we can do better: if we didn't get a "surface enter" we can assume we're on
        the same output */
     set_output (self, NULL);
-    set_height (self, 0);
+    set_height (self, POS_INPUT_SURFACE_DEFAULT_HEIGHT);
     return;
   }
 