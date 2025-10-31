26500696e2d0bfb6f3a3424b75811c18358252d5
Guido Günther <agx@sigxcpu.org>
2025-10-31 12:36:54 +0100

    input-surface: Fix indent
    Gbp-Dch: Ignore
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/251>

--- src/pos-input-surface.c
+++ src/pos-input-surface.c
@@ -173,8 +173,10 @@ static void pos_input_surface_action_group_iface_init (GActionGroupInterface *if
 static void pos_input_surface_action_map_iface_init (GActionMapInterface *iface);
 
 G_DEFINE_TYPE_WITH_CODE (PosInputSurface, pos_input_surface, PHOSH_TYPE_LAYER_SURFACE,
-                         G_IMPLEMENT_INTERFACE (G_TYPE_ACTION_GROUP, pos_input_surface_action_group_iface_init)
-                         G_IMPLEMENT_INTERFACE (G_TYPE_ACTION_MAP, pos_input_surface_action_map_iface_init)
+                         G_IMPLEMENT_INTERFACE (G_TYPE_ACTION_GROUP,
+                                                pos_input_surface_action_group_iface_init)
+                         G_IMPLEMENT_INTERFACE (G_TYPE_ACTION_MAP,
+                                                pos_input_surface_action_map_iface_init)
   )
 
 
