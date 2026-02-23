d70b80ec1b945ca8a9b163279eda3587f094abe0
Guido Günther <agx@sigxcpu.org>
2026-02-23 18:28:57 +0100

    completion-bar: Add mode button
    The button allows to show a mode name and emit a signal
    when pressed. This allows completers to switch their
    current mode. What a mode is depends on the completer.
    
    We put the button at the very end to make it easy to reach.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/245>

--- src/pos-completion-bar.c
+++ src/pos-completion-bar.c
@@ -16,6 +16,13 @@
 #include "pos-completions-box.h"
 
 enum {
+  PROP_0,
+  PROP_MODE_NAME,
+  PROP_LAST_PROP
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+enum {
   SELECTED,
   N_SIGNALS
 };
@@ -32,8 +39,58 @@ struct _PosCompletionBar {
 
   PosCompletionsBox *completions_box;
   GtkScrolledWindow *scrolled_window;
+
+  char * mode_name;
 };
 G_DEFINE_TYPE (PosCompletionBar, pos_completion_bar, GTK_TYPE_BOX)
+
+
+static void
+set_mode_name (PosCompletionBar *self, const char *mode_name)
+{
+  if (!g_set_str (&self->mode_name, mode_name))
+    return;
+
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_MODE_NAME]);
+}
+
+
+static void
+pos_completion_bar_set_property (GObject      *object,
+                                 guint         property_id,
+                                 const GValue *value,
+                                 GParamSpec   *pspec)
+{
+  PosCompletionBar *self = POS_COMPLETION_BAR (object);
+
+  switch (property_id) {
+  case PROP_MODE_NAME:
+    set_mode_name (self, g_value_get_string (value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+pos_completion_bar_get_property (GObject    *object,
+                                 guint       property_id,
+                                 GValue     *value,
+                                 GParamSpec *pspec)
+{
+  PosCompletionBar *self = POS_COMPLETION_BAR (object);
+
+  switch (property_id) {
+  case PROP_MODE_NAME:
+    g_value_set_string (value, self->mode_name);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
 
 
 static void
@@ -46,9 +103,37 @@ on_completion_selected (PosCompletionBar *self, const char *completion)
 
 
 static void
+pos_completion_bar_finalize (GObject *object)
+{
+  PosCompletionBar *self = POS_COMPLETION_BAR (object);
+
+  g_clear_pointer (&self->mode_name, g_free);
+
+  G_OBJECT_CLASS (pos_completion_bar_parent_class)->finalize (object);
+}
+
+
+static void
 pos_completion_bar_class_init (PosCompletionBarClass *klass)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->get_property = pos_completion_bar_get_property;
+  object_class->set_property = pos_completion_bar_set_property;
+  object_class->finalize = pos_completion_bar_finalize;
+
+  /**
+   * PosCompletionBar:mode-name
+   *
+   * Name of the mode displayed on the mode toggle.
+   */
+  props[PROP_MODE_NAME] =
+    g_param_spec_string ("mode-name", "", "",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 
   signals[SELECTED] = g_signal_new ("selected",
                                     G_TYPE_FROM_CLASS (klass),
