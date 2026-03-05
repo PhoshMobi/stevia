PhoshMobi/stevia

commit 38ba8d374a687bcf7d56003c82505c6365fd6b70

input-surface: Allow completer to handle the completion selection
Rather than just submitting the selection from the completion bar
some completers like uim need to handle the submission as they
don't submit the selected text verbatim. Allow them to override it.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/245>

src/pos-completer.c
@@ -37,6 +37,9 @@
  * characters to either take the user input as is or force
  * "aggressive" autocorrection (picking a correction on the users
  * behalf).
+ *
+ * @set_selection: Invoked when the user selected a completion item.
+ *     Implementation of this method is optional.
  */
 
 G_DEFINE_INTERFACE (PosCompleter, pos_completer, G_TYPE_OBJECT)
@@ -335,6 +338,32 @@ pos_completer_set_language (PosCompleter *self,
 
   return iface->set_language (self, lang, region, error);
 }
+
+/**
+ * pos_completer_set_selected:
+ * @self: The completer
+ * @selected:  The selection
+ *
+ * Inform the completer that the user has selected an option provided
+ * by the completer. Returns `TRUE` if the completer handled the selection.
+ *
+ * Returns: %TRUE when handled, otherwise `FALSE`.
+ */
+gboolean
+pos_completer_set_selected (PosCompleter *self, const char *selected)
+{
+  PosCompleterInterface *iface;
+
+  g_return_val_if_fail (POS_IS_COMPLETER (self), FALSE);
+
+  iface = POS_COMPLETER_GET_IFACE (self);
+  /* optional */
+  if (iface->set_selected == NULL)
+    return FALSE;
+
+  return iface->set_selected (self, selected);
+}
+
 
 /* Used by completers to simplify implementations */
 

src/pos-completer.h
@@ -57,6 +57,7 @@ struct _PosCompleterInterface
   char *         (*get_display_name) (PosCompleter *self);
   void           (*learn_accepted) (PosCompleter *self, const char *word);
   const char *   (*lookup_completion) (PosCompleter *self, const char *completion);
+  gboolean       (*set_selected) (PosCompleter *self, const char *selected);
 };
 
 /* Used by completion users */
@@ -80,5 +81,6 @@ GStrv          pos_completer_capitalize_by_template (const char *template,
                                                      const GStrv completions);
 
 glong          pos_completer_find_prev_word_break (const char *text);
+gboolean       pos_completer_set_selected (PosCompleter *self, const char *selected);
 
 G_END_DECLS

src/pos-input-surface.c
@@ -443,13 +443,14 @@ on_completion_selected (PosInputSurface *self, const char *completion)
   else
     send = g_strdup_printf ("%s ", completion);
 
-  pos_input_method_send_string (self->input_method, send, TRUE);
+  /* Some completers like uim need more control than just submitting the selection */
+  if (pos_completer_set_selected (self->completer, completion) == FALSE)
+    pos_input_method_send_string (self->input_method, send, TRUE);
 
   if (pos_input_surface_is_completer_active (self)) {
     pos_completer_learn_accepted (self->completer, send);
     pos_completer_set_preedit (self->completer, NULL);
   }
-
 }
 
 

commit 2fc2b1cc304def1b43d15ccdf1a54f1bc92534b8

completer: Move interface method doc to interface
Avoids a warning when building the docs

Fixes: 38ba8d374 ("input-surface: Allow completer to handle the completion selection")
Fixes: b60a3e82d ("completers: Add mode toggling to interface")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/274>

src/pos-completer.c
@@ -37,11 +37,6 @@
  * characters to either take the user input as is or force
  * "aggressive" autocorrection (picking a correction on the users
  * behalf).
- *
- * @set_selection: Invoked when the user selected a completion item.
- *     Implementation of this method is optional.
- * @toggle_mode: Toggle completion mode.
- *     Implementation of this method is optional.
  */
 
 G_DEFINE_INTERFACE (PosCompleter, pos_completer, G_TYPE_OBJECT)

src/pos-completer.h
@@ -57,7 +57,25 @@ struct _PosCompleterInterface
   char *         (*get_display_name) (PosCompleter *self);
   void           (*learn_accepted) (PosCompleter *self, const char *word);
   const char *   (*lookup_completion) (PosCompleter *self, const char *completion);
+  /**
+   * PosCompleterInterface::set_selected:
+   * @self: The completer
+   * @selected: The selected completion
+   *
+   * Invoked when the user selected a completion item.
+   * Implementation of this method is optional.
+   *
+   * Returns: Whether the completer processed the provided `completion`.
+   */
   gboolean       (*set_selected) (PosCompleter *self, const char *selected);
+
+  /**
+   * PosCompleterInterface::toggle_mode:
+   * @self: The completer
+   *
+   * Toggle completion mode.
+   * Implementation of this method is optional.
+   */
   void           (*toggle_mode) (PosCompleter *self);
 };
 