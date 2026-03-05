PhoshMobi/stevia

commit b60a3e82dc46c8f6460923f588bddf8e5c4e9289

completers: Add mode toggling to interface
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/245>

src/pos-completer.c
@@ -40,6 +40,8 @@
  *
  * @set_selection: Invoked when the user selected a completion item.
  *     Implementation of this method is optional.
+ * @toggle_mode: Toggle completion mode.
+ *     Implementation of this method is optional.
  */
 
 G_DEFINE_INTERFACE (PosCompleter, pos_completer, G_TYPE_OBJECT)
@@ -104,6 +106,14 @@ pos_completer_default_init (PosCompleterInterface *iface)
    */
   g_object_interface_install_property (
     iface, g_param_spec_boxed ("completions", "", "", G_TYPE_STRV, G_PARAM_READABLE));
+
+  /**
+   * PosCompleter:mode-name:
+   *
+   * Mode name shown by the OSK along with the mode toggle.
+   */
+  g_object_interface_install_property (
+    iface, g_param_spec_string ("mode-name", "", "", NULL, G_PARAM_READABLE));
 
   /**
    * PosCompleter::commit-string:
@@ -464,6 +474,27 @@ pos_completer_learn_accepted (PosCompleter *self, const char *word)
     return;
 
   return iface->learn_accepted (self, word);
+}
+
+/**
+ * pos_completer_toggle_mode:
+ * @iface: The completer
+ *
+ * Toggle the completer mode. It's up to the completer to make sense
+ * of mode toggles and to keep track of state.
+ */
+void
+pos_completer_toggle_mode (PosCompleter *self)
+{
+  PosCompleterInterface *iface;
+
+  g_return_if_fail (POS_IS_COMPLETER (self));
+
+  iface = POS_COMPLETER_GET_IFACE (self);
+  if (iface->toggle_mode == NULL)
+    return;
+
+  return iface->toggle_mode (self);
 }
 
 /**

src/completers/pos-completer-fzf.c
@@ -27,6 +27,7 @@ enum {
   PROP_NAME,
   PROP_PREEDIT,
   PROP_COMPLETIONS,
+  PROP_MODE_NAME,
   PROP_LAST_PROP
 };
 static GParamSpec *props[PROP_LAST_PROP];
@@ -167,6 +168,9 @@ pos_completer_fzf_get_property (GObject    *object,
   case PROP_COMPLETIONS:
     g_value_set_boxed (value, self->completions);
     break;
+  case PROP_MODE_NAME:
+    g_value_set_string (value, NULL);
+    break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
     break;
@@ -203,6 +207,9 @@ pos_completer_fzf_class_init (PosCompleterFzfClass *klass)
 
   g_object_class_override_property (object_class, PROP_COMPLETIONS, "completions");
   props[PROP_COMPLETIONS] = g_object_class_find_property (object_class, "completions");
+
+  g_object_class_override_property (object_class, PROP_MODE_NAME, "mode-name");
+  props[PROP_MODE_NAME] = g_object_class_find_property (object_class, "mode-name");
 }
 
 

src/completers/pos-completer-hunspell.c
@@ -27,6 +27,7 @@ enum {
   PROP_NAME,
   PROP_PREEDIT,
   PROP_COMPLETIONS,
+  PROP_MODE_NAME,
   PROP_LAST_PROP
 };
 static GParamSpec *props[PROP_LAST_PROP];
@@ -151,6 +152,9 @@ pos_completer_hunspell_get_property (GObject    *object,
   case PROP_COMPLETIONS:
     g_value_set_boxed (value, self->completions);
     break;
+  case PROP_MODE_NAME:
+    g_value_set_string (value, NULL);
+    break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
     break;
@@ -203,6 +207,9 @@ pos_completer_hunspell_class_init (PosCompleterHunspellClass *klass)
 
   g_object_class_override_property (object_class, PROP_COMPLETIONS, "completions");
   props[PROP_COMPLETIONS] = g_object_class_find_property (object_class, "completions");
+
+  g_object_class_override_property (object_class, PROP_MODE_NAME, "mode-name");
+  props[PROP_MODE_NAME] = g_object_class_find_property (object_class, "mode-name");
 }
 
 

src/completers/pos-completer-pipe.c
@@ -25,6 +25,7 @@ enum {
   PROP_NAME,
   PROP_PREEDIT,
   PROP_COMPLETIONS,
+  PROP_MODE_NAME,
   PROP_LAST_PROP
 };
 static GParamSpec *props[PROP_LAST_PROP];
@@ -139,6 +140,9 @@ pos_completer_pipe_get_property (GObject    *object,
   case PROP_COMPLETIONS:
     g_value_set_boxed (value, self->completions);
     break;
+  case PROP_MODE_NAME:
+    g_value_set_string (value, NULL);
+    break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
     break;
@@ -181,6 +185,9 @@ pos_completer_pipe_class_init (PosCompleterPipeClass *klass)
 
   g_object_class_override_property (object_class, PROP_COMPLETIONS, "completions");
   props[PROP_COMPLETIONS] = g_object_class_find_property (object_class, "completions");
+
+  g_object_class_override_property (object_class, PROP_MODE_NAME, "mode-name");
+  props[PROP_MODE_NAME] = g_object_class_find_property (object_class, "mode-name");
 }
 
 

src/completers/pos-completer-presage.c
@@ -43,6 +43,8 @@ enum {
   PROP_NAME,
   PROP_PREEDIT,
   PROP_COMPLETIONS,
+  PROP_MODE_NAME,
+  /* presage specific */
   PROP_DICT_DIR,
   PROP_LAST_PROP
 };
@@ -365,6 +367,9 @@ pos_completer_presage_get_property (GObject    *object,
   case PROP_COMPLETIONS:
     g_value_set_boxed (value, self->completions);
     break;
+  case PROP_MODE_NAME:
+    g_value_set_string (value, NULL);
+    break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
     break;
@@ -406,6 +411,9 @@ pos_completer_presage_class_init (PosCompleterPresageClass *klass)
 
   g_object_class_override_property (object_class, PROP_COMPLETIONS, "completions");
   props[PROP_COMPLETIONS] = g_object_class_find_property (object_class, "completions");
+
+  g_object_class_override_property (object_class, PROP_MODE_NAME, "mode-name");
+  props[PROP_MODE_NAME] = g_object_class_find_property (object_class, "mode-name");
 
   props[PROP_DICT_DIR] =
     g_param_spec_string ("dict-dir", "", "",

src/completers/pos-completer-varnam.c
@@ -27,6 +27,7 @@ enum {
   PROP_NAME,
   PROP_PREEDIT,
   PROP_COMPLETIONS,
+  PROP_MODE_NAME,
   PROP_LAST_PROP
 };
 static GParamSpec *props[PROP_LAST_PROP];
@@ -145,6 +146,9 @@ pos_completer_varnam_get_property (GObject    *object,
   case PROP_COMPLETIONS:
     g_value_set_boxed (value, self->completions);
     break;
+  case PROP_MODE_NAME:
+    g_value_set_string (value, NULL);
+    break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
     break;
@@ -188,6 +192,9 @@ pos_completer_varnam_class_init (PosCompleterVarnamClass *klass)
 
   g_object_class_override_property (object_class, PROP_COMPLETIONS, "completions");
   props[PROP_COMPLETIONS] = g_object_class_find_property (object_class, "completions");
+
+  g_object_class_override_property (object_class, PROP_MODE_NAME, "mode-name");
+  props[PROP_MODE_NAME] = g_object_class_find_property (object_class, "mode-name");
 }
 
 

src/pos-completer.h
@@ -58,6 +58,7 @@ struct _PosCompleterInterface
   void           (*learn_accepted) (PosCompleter *self, const char *word);
   const char *   (*lookup_completion) (PosCompleter *self, const char *completion);
   gboolean       (*set_selected) (PosCompleter *self, const char *selected);
+  void           (*toggle_mode) (PosCompleter *self);
 };
 
 /* Used by completion users */
@@ -82,5 +83,6 @@ GStrv          pos_completer_capitalize_by_template (const char *template,
 
 glong          pos_completer_find_prev_word_break (const char *text);
 gboolean       pos_completer_set_selected (PosCompleter *self, const char *selected);
+void           pos_completer_toggle_mode (PosCompleter *self);
 
 G_END_DECLS

src/pos-completion-bar.c
@@ -24,6 +24,7 @@ static GParamSpec *props[PROP_LAST_PROP];
 
 enum {
   SELECTED,
+  MODE_PRESSED,
   N_SIGNALS
 };
 static guint signals[N_SIGNALS];
@@ -103,6 +104,15 @@ on_completion_selected (PosCompletionBar *self, const char *completion)
 
 
 static void
+on_mode_button_clicked (PosCompletionBar *self)
+{
+  g_assert (POS_IS_COMPLETION_BAR (self));
+
+  g_signal_emit (self, signals[MODE_PRESSED], 0);
+}
+
+
+static void
 pos_completion_bar_finalize (GObject *object)
 {
   PosCompletionBar *self = POS_COMPLETION_BAR (object);
@@ -143,6 +153,13 @@ pos_completion_bar_class_init (PosCompletionBarClass *klass)
                                     1,
                                     G_TYPE_STRING);
 
+  signals[MODE_PRESSED] = g_signal_new ("mode-pressed",
+                                        G_TYPE_FROM_CLASS (klass),
+                                        G_SIGNAL_RUN_LAST,
+                                        0, NULL, NULL, NULL,
+                                        G_TYPE_NONE,
+                                        0);
+
   g_type_ensure (POS_TYPE_COMPLETIONS_BOX);
 
   gtk_widget_class_set_template_from_resource (widget_class,
@@ -151,6 +168,7 @@ pos_completion_bar_class_init (PosCompletionBarClass *klass)
   gtk_widget_class_bind_template_child (widget_class, PosCompletionBar, scrolled_window);
 
   gtk_widget_class_bind_template_callback (widget_class, on_completion_selected);
+  gtk_widget_class_bind_template_callback (widget_class, on_mode_button_clicked);
 
   gtk_widget_class_set_css_name (widget_class, "pos-completion-bar");
 }

commit 50888b074f058842278aa9eb09552578bd420fc0

completer: Fix doc args
Fixes: b60a3e82d ("completers: Add mode toggling to interface")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/274>

src/pos-completer.c
@@ -451,7 +451,7 @@ pos_completer_get_display_name (PosCompleter *self)
 
 /**
  * pos_completer_learn_accepted:
- * @iface: The completer iface
+ * @self: The completer iface
  * @word: The word to learn
  *
  * The completer should learn the given word and offer it for
@@ -473,7 +473,7 @@ pos_completer_learn_accepted (PosCompleter *self, const char *word)
 
 /**
  * pos_completer_toggle_mode:
- * @iface: The completer
+ * @self: The completer
  *
  * Toggle the completer mode. It's up to the completer to make sense
  * of mode toggles and to keep track of state.