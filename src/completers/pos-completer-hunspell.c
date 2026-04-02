PhoshMobi/stevia

commit 78c6ef62136cc804dd0eb45566abfadc4318e7bb

completers/hunspell: Try to be clever when inserting punctuation
If the preceding char is a space we drop it as that usually means
a completed word was inserted.

Closes: https://gitlab.gnome.org/World/Phosh/stevia/-/issues/30
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/238>

src/completers/pos-completer-hunspell.c
@@ -360,7 +360,16 @@ pos_completer_hunspell_feed_symbol (PosCompleter *iface, const char *symbol)
   int ret;
 
   if (pos_completer_add_preedit (POS_COMPLETER (self), self->preedit, symbol)) {
-    g_signal_emit_by_name (self, "commit-string", self->preedit->str, 0, 0);
+    PosCompleterBase *base = POS_COMPLETER_BASE (self);
+    int before = 0;
+
+    if (pos_completer_base_wants_punctuation_swap (base, symbol) &&
+        /* Only swap if preedit is symbol + space */
+        self->preedit->len == 2) {
+      before = 1;
+    }
+
+    g_signal_emit_by_name (self, "commit-string", self->preedit->str, before, 0);
     pos_completer_hunspell_set_preedit (POS_COMPLETER (self), NULL);
 
     /* Make sure enter is processed as raw keystroke */

commit 98f6bcaa877082875836146c24d3d944685b0948

hunspell: Set surrounding text
This is needed for automatic space ↔ punctuation swap

Fixes: Fixes: 78c6ef621 ("completers/hunspell: Try to be clever when inserting punctuation")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/stevia/-/merge_requests/282>

src/completers/pos-completer-hunspell.c
@@ -116,6 +116,19 @@ pos_completer_hunspell_set_preedit (PosCompleter *iface, const char *preedit)
 
 
 static void
+pos_completer_hunspell_set_surrounding_text (PosCompleter *iface,
+                                             const char   *before_text,
+                                             const char   *after_text)
+{
+  PosCompleterHunspell *self = POS_COMPLETER_HUNSPELL (iface);
+
+  pos_completer_base_set_surrounding_text (POS_COMPLETER_BASE (self),
+                                           before_text,
+                                           after_text);
+}
+
+
+static void
 pos_completer_hunspell_set_property (GObject      *object,
                                      guint         property_id,
                                      const GValue *value,
@@ -421,6 +434,7 @@ pos_completer_hunspell_interface_init (PosCompleterInterface *iface)
   iface->feed_symbol = pos_completer_hunspell_feed_symbol;
   iface->get_preedit = pos_completer_hunspell_get_preedit;
   iface->set_preedit = pos_completer_hunspell_set_preedit;
+  iface->set_surrounding_text = pos_completer_hunspell_set_surrounding_text;
   iface->set_language = pos_completer_hunspell_set_language;
 }
 